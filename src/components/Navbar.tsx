86342a0713c46839965c9aa8377c1ce0ebc58a48
Harithamolvh <vhharithamol@gmail.com>
2026-01-11 22:14:26 +0530

    home page design update

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,9 +1,11 @@
+import '../assets/MainBanner.css';
+import logo from '../assets/MainBannerImg/icon.png';
 const Navbar = () => {
     return(
         <>
-            <nav className="navbar navbar-expand-lg bg-body-tertiary">
+            <nav className="navbar navbar-expand-lg bg-navbar">
             <div className="container-fluid">
-                <a className="navbar-brand" href="#">Colours</a>
+                <a className="navbar-brand" href="#"><img className="w-15" src={logo}/></a>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
                 </button>
---
+++ src/components/Service.tsx
@@ -0,0 +1,44 @@
+const Service = () =>{
+    interface serviceArray{
+        id:number;
+        serviceName:string;
+        description:string;
+        icon:string
+    }
+
+    const array =[
+        {
+            id:1,
+            serviceName:"Career Consultation",
+            icon:"",
+            description:"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. "
+        },
+        {
+            id:2,
+            serviceName:"Upskill",
+            icon:"",
+            description:"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. "
+        },
+        {
+            id:3,
+            serviceName:"Education Abroad",
+            icon:"",
+            description:"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. "
+        },
+        {
+            id:4,
+            serviceName:"Competative Exams",
+            icon:"",
+            description:"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. "
+        },
+    ];
+
+    return(
+        <>
+
+        </>
+    );
+
+}
+
+export default Service;
